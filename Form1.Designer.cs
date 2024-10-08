c82a829b7caf3e4bf5788705780a95d6724a86b0
DanilloMatavelli <danillomatavelli26@gmail.com>
2024-10-08 13:48:07 -0300

    Criado as janelas de login e cadastro e programado as validações primárias

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            txtSenha = new TextBox();
+            txtUsuario = new TextBox();
+            btnCadastrar = new Button();
+            btnEntrar = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(28, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Usuário";
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(28, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(28, 130);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PasswordChar = '*';
+            txtSenha.Size = new Size(183, 23);
+            txtSenha.TabIndex = 2;
+            txtSenha.TextChanged += txtSenha_TextChanged;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(28, 68);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(183, 23);
+            txtUsuario.TabIndex = 3;
+            txtUsuario.TextChanged += txtUsuario_TextChanged;
+            // 
+            // btnCadastrar
+            // 
+            btnCadastrar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCadastrar.Location = new Point(120, 172);
+            btnCadastrar.Name = "btnCadastrar";
+            btnCadastrar.Size = new Size(91, 32);
+            btnCadastrar.TabIndex = 4;
+            btnCadastrar.Text = "CADASTRAR";
+            btnCadastrar.UseVisualStyleBackColor = true;
+            btnCadastrar.Click += btnCadastrar_Click;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Enabled = false;
+            btnEntrar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEntrar.Location = new Point(28, 172);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(91, 32);
+            btnEntrar.TabIndex = 5;
+            btnEntrar.Text = "ENTRAR";
+            btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.Location = new Point(28, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(183, 23);
+            label3.TabIndex = 6;
+            label3.Text = "LOGIN";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(415, 227);
+            Controls.Add(label3);
+            Controls.Add(btnEntrar);
+            Controls.Add(btnCadastrar);
+            Controls.Add(txtUsuario);
+            Controls.Add(txtSenha);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtSenha;
+        private TextBox txtUsuario;
+        private Button btnCadastrar;
+        private Button btnEntrar;
+        private Label label3;
     }
 }
---
+++ FrmCadastrar.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoAgenda
+{
+    public partial class FrmCadastrar : Form
+    {
+        public FrmCadastrar()
+        {
+            InitializeComponent();
+        }
+
+        private void habilitarBotaoCadastrar()
+        {
+            // Se o usuário for diferente de vazio e e a quantidade de caracteres na senha for maior ou igual a 8, habilita o botão de login
+            // Para verificar quantos caracteres tem no textBox
+            if (txtNome.Text != "" && txtUsuario.Text != "" && txtSenha.Text.Length >= 8 && txtRepitaSenha.Text.Length >= 8 && txtSenha.Text.Length == txtRepitaSenha.Text.Length)
+            {
+                btnCadastrar.Enabled = true;
+            }
+
+            else
+            {
+                btnCadastrar.Enabled = false;
+            }
+
+            // OUTRO JEITO DE FAZER
+            // private void habilitarBotaoCadastrar()
+            //{
+            //bool erro = false;
+
+            //VERIFICA O NOME
+            //if (txtNome.Text == "")
+            //{
+                //erro = true;
+            //}
+
+            //VERIFICA O USUÁRIO
+            //if (txtUsuario.Text == "")
+            //{
+                //erro = true;
+            //}
+
+            //VERIFICA SENHA
+            //if (txtSenha.Text.Length < 8)
+            //{
+                //erro = true;
+            //}
+
+            //VERIFICA REPETE SENHA
+            // if (txtSenha.Text != txtRepitaSenha.Text)
+            //{
+                //erro = true;
+            //}
+
+            //CASO NÃO ENCONTRE NENHUM ERRO HABILITA O BOTÃO CADASTRAR
+            //if (erro == false)
+            //{
+                //btnCadastrar.Enabled = true;
+            //}
+
+            //else
+            //{
+                //btnCadastrar.Enabled = false;
+            //}
+            //}
+
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        private void txtUsuario_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        private void txtRepitaSenha_TextChanged(object sender, EventArgs e)
+        {
+            habilitarBotaoCadastrar();
+        }
+
+        
+    }
+}
